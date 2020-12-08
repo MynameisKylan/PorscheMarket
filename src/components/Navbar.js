9418c9174def1a5dbd6c49c4a4b30270c64548f3
Kylan Wong <mynameiskylan@gmail.com>
2020-12-08 15:32:37 -0800

    setup routes in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from './Home';
+import Shop from './Shop';
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <nav>
+        <Link to="/">AK Skin Mart</Link>
+        <ul className='navlinks'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/shop' component={Shop} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default Navbar
+export default Navbar;
