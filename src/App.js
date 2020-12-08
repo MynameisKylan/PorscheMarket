953adc347b658edec06942d0becb4d47019b0faa
Kylan Wong <mynameiskylan@gmail.com>
2020-12-08 15:23:12 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
