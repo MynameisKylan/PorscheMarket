a147273dd803d3ee2fc380dc809ef6a8479da3cd
Kylan Wong <mynameiskylan@gmail.com>
2020-12-10 11:09:22 -0800

    more config for gh pages

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,7 +5,7 @@ function Checkout(props) {
     <div className='content'>
       <h1>Checkout</h1>
       <h1>Page Currently Under Construction</h1>
-      <img src='https://s1.cdn.autoevolution.com/images/news/gallery/watch-shmee150-s-gelbgrn-2018-porsche-911-gt3-being-built-in-zuffenhausen_3.jpg'></img>
+      <img src='https://s1.cdn.autoevolution.com/images/news/gallery/watch-shmee150-s-gelbgrn-2018-porsche-911-gt3-being-built-in-zuffenhausen_3.jpg' alt='a 911 GT3 being built'></img>
     </div>
   )
 }
